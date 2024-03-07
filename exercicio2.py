e5560399d79288e565444f96d5c2a1ee048715af
fernanda <fernandarakell77@gmail.com>
2024-03-07 20:21:28 -0300

    meu commit

---
+++ exercicio2.py
@@ -0,0 +1,4 @@
+dado = input ("digite seu nome e a data de nascimento e a data de nascimento separado por ';':")
+cadastro = dado.split(";")
+
+print (f"Nome {cadastro[0]} \n Data de nascimento {cadastro[1]}")
