71a820639cf63d55b2f15f56af4f19af83993485
fernanda <fernandarakell77@gmail.com>
2024-03-21 20:32:59 -0300

    meu commit

--- exercicio10.py
+++ exercicio10.py
@@ -1,7 +1,14 @@
+from pprint import pprint
 lista = [ ]
 for i in range (4):
     tempo = int(input("digite um ano:"))
-    if tempo % 4 == 0 and tempo % 100 != 0 or tempo % 400 == 0: 
-        print(f"o ano {tempo} é bissexto")
+    if (tempo % 4 == 0 and tempo % 100 != 0) or tempo % 400 == 0: 
+        bissexto = True
+        x = {"ano": tempo, "bissexto": bissexto}
+        lista.append(x)
     else :
-        print(f"o ano {tempo} não é bissexto")
+        bissexto = False
+        x = {"ano": tempo, "bissexto": bissexto}
+        lista.append(x)
+
+pprint(lista)
