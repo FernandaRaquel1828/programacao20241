db08f8439493c5a928ee36ea7e18a5fd1b8eb15f
FernandaRaquel1828 <105254237+FernandaRaquel1828@users.noreply.github.com>
2024-03-21 10:54:20 -0300

    Update exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -1,8 +1,12 @@
+x = {"ano": tempo, "bissexto": bissexto}
 lista = [ ]
 for i in range (4):
     tempo = int(input("digite um ano:"))
     if tempo % 4 == 0 and tempo % 100 != 0 or tempo % 400 == 0: 
         bissexto = True
+        lista.append(x)
     else :
         bissexto = False
-        x = {"ano": tempo, "bisexto": bissexto}
+        lista.append(x)
+
+print(lista)
