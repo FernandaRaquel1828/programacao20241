0559486e8c3388a860d03c596090c3a700f5b013
fernada raquel <fernadarakell77@gmail.com>
2024-04-04 15:26:02 -0300

    commits

--- atividade2.py
+++ atividade2.py
@@ -1,9 +1,6 @@
 lista = []
 maiorque170 = []
-def ord_peso(item):
-  return("peso")
-def ord_imc(item2):
-  return("IMC")
+
 while True: 
   pessoa = input("digite nome, peso e altura do usuário ")
   nome = pessoa.split("-")[0]
@@ -12,15 +9,24 @@ while True:
   cadastro = {"nome": nome ,"peso":dados[0] ,"altura": dados[1], "IMC": imc }
   lista.append(cadastro)
   sair = input("deseja sair? digite nao para continuar ou FIM para sair").upper()
+
   if sair == "FIM":
     break
+
 for i in lista:
     if i["altura"] > 1.70:
         maiorque170.append(i)
 
-cad = sorted(lista, key = ord_peso)
-imc1 = sorted(lista,key = ord_imc)
-print(lista)
-print(imc1)
-print(cad)
+print('\nLISTA:')
+print(*lista, sep='\n')
+
+print('\nIMC:')
+sorted_imc = sorted(lista, key=lambda x: x['IMC'])
+print(*sorted_imc, sep='\n')
+
+print('\nPeso:')
+sorted_cad = sorted(lista, key=lambda x: x['peso'])
+print(*sorted_cad, sep='\n')
+
+print('\nMAIOR QUE 70:')
 print(maiorque170)
